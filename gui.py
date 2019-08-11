ab3a4a20538ff42f67c5bfc1e2c92c13be17a516
Darrion Singh <1056673@students.wits.ac.za>
2019-08-11 11:50:50 +0200

    GUI first window complete

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 import os
+from PIL import Image, ImageTk
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -32,6 +33,15 @@ class Application(tk.Frame):
         self.ChooseSideImageButton["text"] = "Browse Files ..."
         self.ChooseSideImageButton.grid(row=1, column=2)
 
+        self.FrontPhotoFrame = tk.Frame(self.SelectImageFrame)
+        self.FrontPhotoFrame.grid(row=0, column=3)
+        self.FrontPhotoLabel = tk.Label(self.FrontPhotoFrame, text="No File Selected.", image="")
+        self.FrontPhotoLabel.pack()
+        self.SidePhotoFrame = tk.Frame(self.SelectImageFrame)
+        self.SidePhotoFrame.grid(row=1, column=3)
+        self.SidePhotoLabel = tk.Label(self.SidePhotoFrame, text="No File Selected.", image="")
+        self.SidePhotoLabel.pack(side=tk.BOTTOM)
+
         self.UseOnlyFrontImageTickBox = tk.Checkbutton(self.CheckBoxFrame, text="Only Use Front Image", variable=UseOnlyFrontImage, command=self.UseOnlyOneImage)
         self.UseOnlyFrontImageTickBox.pack()
         self.UseOnlySideImageTickBox = tk.Checkbutton(self.CheckBoxFrame, text="Only Use Side Image", variable=UseOnlySideImage, command=self.UseOnlyOneImage)
@@ -45,28 +55,49 @@ class Application(tk.Frame):
 
     def UseOnlyOneImage(self):
         if UseOnlyFrontImage.get():
-            [slave.config(state=tk.DISABLED) for slave in self.SelectImageFrame.grid_slaves() if slave.grid_info()['row']== 1]
+            [slave.config(state=tk.DISABLED) for slave in self.SelectImageFrame.grid_slaves()[:-1] if slave.grid_info()['row']== 1 and slave.winfo_class() != 'Frame']
             self.UseOnlySideImageTickBox.config(state=tk.DISABLED)
         else:
-            [slave.config(state=tk.NORMAL) for slave in self.SelectImageFrame.grid_slaves() if slave.grid_info()['row']== 1]
+            [slave.config(state=tk.NORMAL) for slave in self.SelectImageFrame.grid_slaves()[:-1] if slave.grid_info()['row']== 1 and slave.winfo_class() != 'Frame']
             self.UseOnlySideImageTickBox.config(state=tk.NORMAL)
         
         if UseOnlySideImage.get():
-            [slave.config(state=tk.DISABLED) for slave in self.SelectImageFrame.grid_slaves() if slave.grid_info()['row']== 0]
+            [slave.config(state=tk.DISABLED) for slave in self.SelectImageFrame.grid_slaves()[:-1] if slave.grid_info()['row']== 0 and slave.winfo_class() != 'Frame']
             self.UseOnlyFrontImageTickBox.config(state=tk.DISABLED)
         else:
-            [slave.config(state=tk.NORMAL) for slave in self.SelectImageFrame.grid_slaves() if slave.grid_info()['row']== 0]
+            [slave.config(state=tk.NORMAL) for slave in self.SelectImageFrame.grid_slaves()[:-1] if slave.grid_info()['row']== 0 and slave.winfo_class() != 'Frame']
             self.UseOnlyFrontImageTickBox.config(state=tk.NORMAL)
     
     def returnFileName(self):
         fileName = filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select file",filetypes = (("jpg files","*.jpg"), ("jpeg files","*.jpeg"),("all files","*.*")))
-        return fileName
+        if fileName != "": return fileName
+        else: return "No File Selected."
+
+    def chooseFile(self, textbox, label, is_front):
+        fileName = self.returnFileName()
+        if fileName == "No File Selected.":
+            return
+        textbox.insert(tk.INSERT, fileName)
+        image = Image.open(fileName)
+        image.thumbnail((100,100))
+        if is_front:
+            self.FrontPhoto = ImageTk.PhotoImage(image)
+            label.config(text=fileName, image=self.FrontPhoto)
+        else:
+            self.SidePhoto = ImageTk.PhotoImage(image)
+            label.config(text=fileName, image=self.SidePhoto)
+        label.pack(side=tk.BOTTOM)
     
     def chooseFrontFile(self):
-        self.FrontImageTextBox.insert(tk.INSERT, self.returnFileName())
+        FrontFileName.set("")
+        self.chooseFile(self.FrontImageTextBox, self.FrontPhotoLabel, True)
+        return
     
     def chooseSideFile(self):
-        self.SideImageTextBox.insert(tk.INSERT, self.returnFileName())
+        SideFileName.set("")
+        self.chooseFile(self.SideImageTextBox, self.SidePhotoLabel, False)
+        return
+        
 
 root = tk.Tk()
 UseOnlyFrontImage = tk.BooleanVar()
