fa54b04674a19e21826254784dc962c22fbc1e17
Darrion Singh <1056673@students.wits.ac.za>
2019-08-11 04:20:57 +0200

    Can get file directory

--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import filedialog
+import os
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -18,15 +20,15 @@ class Application(tk.Frame):
         self.SideImageLabel = tk.Label(self.SelectImageFrame, text="Side Image:")
         self.SideImageLabel.grid(row=1)
 
-        self.FrontImageTextBox = tk.Entry(self.SelectImageFrame)
+        self.FrontImageTextBox = tk.Entry(self.SelectImageFrame, textvariable=FrontFileName)
         self.FrontImageTextBox.grid(row=0, column=1)
-        self.SideImageTextBox = tk.Entry(self.SelectImageFrame)
+        self.SideImageTextBox = tk.Entry(self.SelectImageFrame, textvariable=SideFileName)
         self.SideImageTextBox.grid(row=1, column=1)
 
-        self.ChooseFrontImageButton = tk.Button(self.SelectImageFrame)
+        self.ChooseFrontImageButton = tk.Button(self.SelectImageFrame, command=self.chooseFrontFile)
         self.ChooseFrontImageButton["text"] = "Browse Files ..."
         self.ChooseFrontImageButton.grid(row=0, column=2)
-        self.ChooseSideImageButton = tk.Button(self.SelectImageFrame)
+        self.ChooseSideImageButton = tk.Button(self.SelectImageFrame, command=self.chooseSideFile)
         self.ChooseSideImageButton["text"] = "Browse Files ..."
         self.ChooseSideImageButton.grid(row=1, column=2)
 
@@ -35,9 +37,11 @@ class Application(tk.Frame):
         self.UseOnlySideImageTickBox = tk.Checkbutton(self.CheckBoxFrame, text="Only Use Side Image", variable=UseOnlySideImage, command=self.UseOnlyOneImage)
         self.UseOnlySideImageTickBox.pack()
 
-        self.StartProgram = tk.Button(self)
-        self.StartProgram["text"] = "Predict BMI"
+        self.StartProgram = tk.Button(self, text="Predict BMI", command=self.start)
         self.StartProgram.pack(side=tk.BOTTOM)
+
+    def start(self):
+        print(FrontFileName.get(), SideFileName.get())
 
     def UseOnlyOneImage(self):
         if UseOnlyFrontImage.get():
@@ -53,10 +57,22 @@ class Application(tk.Frame):
         else:
             [slave.config(state=tk.NORMAL) for slave in self.SelectImageFrame.grid_slaves() if slave.grid_info()['row']== 0]
             self.UseOnlyFrontImageTickBox.config(state=tk.NORMAL)
+    
+    def returnFileName(self):
+        fileName = filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select file",filetypes = (("jpg files","*.jpg"), ("jpeg files","*.jpeg"),("all files","*.*")))
+        return fileName
+    
+    def chooseFrontFile(self):
+        self.FrontImageTextBox.insert(tk.INSERT, self.returnFileName())
+    
+    def chooseSideFile(self):
+        self.SideImageTextBox.insert(tk.INSERT, self.returnFileName())
 
 root = tk.Tk()
 UseOnlyFrontImage = tk.BooleanVar()
 UseOnlySideImage = tk.BooleanVar()
+FrontFileName = tk.StringVar()
+SideFileName = tk.StringVar()
 root.title("Digital BMI Estimator")
 app = Application(master=root)
 app.mainloop()
