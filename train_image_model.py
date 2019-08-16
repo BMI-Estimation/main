13837679d2df54991bd61c5f5cc7c5e3df03438d
Darrion Singh <1056673@students.wits.ac.za>
2019-08-16 10:44:49 +0200

    Added argument parser

--- train_image_model.py
+++ train_image_model.py
@@ -1,3 +1,27 @@
+import argparse
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--number", type=int, required=True, help="Number of iterations.")
+ap.add_argument("-f", "--front", nargs='?', const=True, type=bool, required=False, default=False, help="Train Front Model.")
+ap.add_argument("-s", "--side", nargs='?', const=True, type=bool, required=False, default=False, help="Train Side Model.")
+ap.add_argument("-v", "--visualize", nargs='?', const=True, type=bool, required=False, default=False, help="View Training Graphs.")
+args = vars(ap.parse_args())
+
+if args["side"]:
+    file_ = "side.csv"
+    Final_Model_File = "Final_Model_Side.h5"
+    Low_Score_File = "Lowest_Score_Side.txt"
+    Classic_Model_File = 'Classical_Side.h5'
+    Cross_Model_File = 'Cross_Side.h5'
+elif args["front"]:
+    file_ = "front.csv"
+    Final_Model_File = "Final_Model_Front.h5"
+    Low_Score_File = "Lowest_Score_Front.txt"
+    Classic_Model_File = 'Classical_Front.h5'
+    Cross_Model_File = 'Cross_Front.h5'
+else:
+    print("Please Specify Front or Side Model training.")
+    exit()
+
 import numpy as np
 import pandas
 from keras.models import Sequential
@@ -13,43 +37,40 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import max_error
 import csv
-# Decide on model to train
-view = "side"
-if view == "side":
-    file = "Side_Model.csv"
-    Final_Model_File = "Final_Model_Side.h5"
-    Low_Score_File = "Lowest_Score_Side.txt"
-    Classic_Model_File = 'Classical_Side.h5'
-    Cross_Model_File = 'Cross_Side.h5'
-else:
-    file = "Front_Model.csv"
-    Final_Model_File = "Final_Model_Front.h5"
-    Low_Score_File = "Lowest_Score_Front.txt"
-    Classic_Model_File = 'Classical_Front.h5'
-    Cross_Model_File = 'Cross_Front.h5'
+
+# Model selection
+def overallscore(MSE, MAE, Max):
+    score = (0.35 * MSE) + (0.35 * MAE) + (0.3 * Max)
+    return score
+
 # load dataset inputs
-dataframe_traning_inputs = open(file, 'r')
+dataframe_traning_inputs = open(file_, 'r')
 reader = csv.reader(dataframe_traning_inputs, delimiter=",")
-Input_parameters = [[int(entry) for entry in row] for row in reader]
+Input_parameters = [[float(entry) for entry in row] for row in reader]
+badDataIndex = [index for index, row in enumerate(Input_parameters) if row[1] > 2.5]
+Input_parameters = [row for index, row in enumerate(Input_parameters) if index not in badDataIndex]
 Input_parameters = np.asarray(Input_parameters)
 #load dataset BMI
 BMI_file = "BMI.csv"
 dataframe_traning_outputs = open(BMI_file, 'r')
 reader = csv.reader(dataframe_traning_outputs, delimiter=",")
-BMI = [[int(entry) for entry in row] for row in reader]
+BMI = [[float(entry) for entry in row] for row in reader]
+BMI = [row for index, row in enumerate(BMI) if index not in badDataIndex]
+# print(Input_parameters)
 BMI = np.asarray(BMI)
+BMI = BMI[:,2]
 # split into input (X) and output (Y) variables
 X = Input_parameters
 Y = BMI
-print(X)
+# print(X)
 Y= Y.reshape(-1,1)
-print(Y)
+# print(Y)
 # define base model
 def baseline_model():
 	# create model
     regressor = Sequential()
-    regressor.add(Dense(units=20, input_dim=6, activation="relu"))
-    regressor.add(Dense(units=4, activation="relu"))
+    regressor.add(Dense(units=7, input_dim=6, activation="relu"))
+    regressor.add(Dense(units=7, activation="relu"))
     regressor.add(Dense(units=1, activation="linear"))
     regressor.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae', 'mse'])
     return regressor
@@ -65,35 +86,14 @@ seed = 10
 np.random.seed(seed)
 X,X_Unseen,Y,Y_Unseen = train_test_split(X,Y,test_size=0.2)
 #finding optimal model
-for x in range(1):
+for x in range(args["number"]):
     np.random.seed(x)
     # classic test split
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
     history = Regressor.fit(X_train, Y_train, batch_size=5, epochs=500, verbose=1, validation_data=(X_test, Y_test))
-    # loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train','test'], loc='upper left')
-    plt.show()
-    # mean absolute error
-    plt.plot(history.history['mae'])
-    plt.plot(history.history['val_mae'])
-    plt.title('Mean absolute error')
-    plt.ylabel('MAE')
-    plt.xlabel('epoch')
-    plt.legend(['train','test'], loc='upper left')
-    plt.show()
+
     Y_Classic = Regressor.predict(X_test)
-    print(Y_Classic)
-    plt.scatter(Y_test, Y_Classic)
-    plt.plot([Y_test.min(), Y_test.max()], [Y_test.min(), Y_test.max()], 'k--', lw=4)
-    plt.xlabel('Measured')
-    plt.ylabel('Predicted')
-    plt.title('Classic method prediction')
-    plt.show()
+    # print(Y_Classic)
     #evaluate model with dataset
     estimator = KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=10, verbose=1)
     Regressor.model.save(Classic_Model_File)
@@ -102,13 +102,7 @@ for x in range(1):
     results = cross_val_score(estimator, X, Y, cv=kfold)
     print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
     print(results)
-    #Cross validation model analysis (loss)
-    plt.plot(abs(results))
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('Fold')
-    plt.legend(['train'], loc='upper left')
-    plt.show()
+
     # Optimal estimator extraction
     Cross_Val = cross_validate(estimator, X, Y, cv=kfold, return_estimator=True)
     estimator_array = Cross_Val['estimator']
@@ -117,19 +111,11 @@ for x in range(1):
     # Assessing optimal model
     Final = load_model(Cross_Model_File)
     Y_Cross_Val = Final.predict(X_test)
-    plt.plot(Y_test)
-    plt.plot(Y_Classic)
-    plt.plot(Y_Cross_Val)
-    plt.legend(['Actual','Classical','Cross Validation'], loc='upper left')
-    plt.show()
+
     # unseen data tests
     Y_Unseen_Classical = Regressor.predict(X_Unseen)
     Y_Unseen_Cross = Final.predict(X_Unseen)
-    plt.plot(Y_Unseen)
-    plt.plot(Y_Unseen_Classical)
-    plt.plot(Y_Unseen_Cross)
-    plt.legend(['Actual','Classical','Cross Validation'], loc='upper left')
-    plt.show()
+
     # Performance indicators
     Classical_MSE = mean_squared_error(Y_Unseen, Y_Unseen_Classical)
     Cross_MSE = mean_squared_error(Y_Unseen, Y_Unseen_Cross)
@@ -140,13 +126,6 @@ for x in range(1):
     print(str(Classical_MSE) + "\t" + str(Cross_MSE))
     print(str(Classical_MAE) + "\t" + str(Cross_MAE))
     print(str(Classical_Max) + "\t" + str(Cross_Max))
-
-
-    # Model selection
-    def overallscore(MSE, MAE, Max):
-        score = (0.35 * MSE) + (0.35 * MAE) + (0.3 * Max)
-        return score
-
 
     Classical_Overall = overallscore(Classical_MSE, Classical_MAE, Classical_Max)
     Cross_Overall = overallscore(Cross_MSE, Cross_MAE, Cross_Max)
@@ -164,14 +143,70 @@ for x in range(1):
             lowest_score = Cross_Overall
             print("new model "+ str(lowest_score))
             progress.append(lowest_score)
+
+
+    if args["visualize"]:
+        # Cross validation model analysis (loss)
+        plt.plot(abs(results))
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('Fold')
+        plt.legend(['train'], loc='upper left')
+
+        plt.plot(Y_Unseen)
+        plt.plot(Y_Unseen_Classical)
+        plt.plot(Y_Unseen_Cross)
+        plt.legend(['Actual','Classical','Cross Validation'], loc='upper left')
+
+        plt.plot(Y_test)
+        plt.plot(Y_Classic)
+        plt.plot(Y_Cross_Val)
+        plt.legend(['Actual','Classical','Cross Validation'], loc='upper left')
+        # loss
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train','test'], loc='upper left')
+        # mean absolute error
+        # print(history.history)
+        plt.plot(history.history['mean_absolute_error'])
+        plt.plot(history.history['val_mean_absolute_error'])
+        plt.title('Mean absolute error')
+        plt.ylabel('MAE')
+        plt.xlabel('epoch')
+        plt.legend(['train','test'], loc='upper left')
+
+        plt.scatter(Y_test, Y_Classic)
+        plt.plot([Y_test.min(), Y_test.max()], [Y_test.min(), Y_test.max()], 'k--', lw=4)
+        plt.xlabel('Measured')
+        plt.ylabel('Predicted')
+        plt.title('Classic method prediction')
+
+        plt.scatter(Y_test, Y_Cross_Val)
+        plt.plot([Y_test.min(), Y_test.max()], [Y_test.min(), Y_test.max()], 'k--', lw=4)
+        plt.xlabel('Measured')
+        plt.ylabel('Predicted')
+        plt.title('Cross val method prediction')
+        plt.show()
+
+
 print(progress)
 Proposed_Model = load_model(Final_Model_File)
 Y_Proposed = Proposed_Model.predict(X_Unseen)
-plt.plot(Y_Unseen)
-plt.plot(Y_Proposed)
-plt.legend(['Actual', 'Model'], loc='upper left')
-plt.show()
+# plt.plot(Y_Unseen)
+# plt.plot(Y_Proposed)
+# plt.legend(['Actual', 'Model'], loc='upper left')
+# plt.show()
 score_file = open(Low_Score_File,"w")
 score_file.write(str(lowest_score))
 score_file.close()
 print(lowest_score)
+
+plt.scatter(Y_Proposed, Y_Unseen)
+plt.plot([Y_Proposed.min(), Y_Proposed.max()], [Y_Proposed.min(), Y_Proposed.max()], 'k--', lw=4)
+plt.xlabel('Measured')
+plt.ylabel('Predicted')
+plt.title('Final Model')
+plt.show()
