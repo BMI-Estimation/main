78f82037d2e2b91fb1224e568c24bc474074e3d2
Sachin Govender <sachingovender39@gmail.com>
2019-09-06 00:04:50 +0200

    final analysis script

--- Analysis.py
+++ Analysis.py
@@ -85,6 +85,39 @@ def MakeBar(Values,Categories,Model,directory,Real):
 	fig.savefig(directory+Model+'.png')
 	plt.show()
 	plt.clf()
+def ErrorAnalysis(Actual,Prediction,length,Dataset,Model):
+	Pred_list = []
+	for x in range(length):
+		Pred_list.append(Prediction[x][0])
+	Pred_list = np.asarray(Pred_list)
+	diff = np.abs((Actual - Pred_list))
+	diff = (diff / Actual) * 100
+	diff = diff[(diff <= 10000)]
+	#create and save histogram
+	plt.hist(diff,bins=10)
+	plt.title('BMI Estimation Error for ' + Model + ' Model on '+Dataset+' Participants')
+	plt.ylabel('Number of participants', fontsize=10)
+	plt.xlabel('Percentage Error', fontsize=7)
+	fig = plt.gcf()
+	fig.savefig(directory+Model+'_'+Dataset+'_'+'Hist.png')
+	plt.show()
+	plt.clf()
+def TotalError(Actual,Prediction,Model):
+	diff = np.abs((Actual - Prediction))
+	diff = (diff / Actual) * 100
+	print(diff[0:50])
+	diff = diff[(diff <= 10000)]
+	# print(len(diff))
+	# print(diff[0:10])
+	#create and save histogram
+	plt.hist(diff,bins=15)
+	plt.title('BMI Estimation Error for ' + Model + ' Model on All Participants')
+	plt.ylabel('Number of participants', fontsize=10)
+	plt.xlabel('Percentage Error', fontsize=7)
+	fig = plt.gcf()
+	fig.savefig(directory+Model+'_Total_'+'Hist.png')
+	plt.show()
+	plt.clf()
 #Underweight check
 Underweight = BMI.copy()
 Underweight=[[i,row[2]]for i,row in enumerate(Underweight) if row[2]<18.5]
@@ -161,3 +194,37 @@ MakeBar(Accuracy_Front,Category,'Front',directory,False)
 MakeBar(Accuracy_Side,Category,'Side',directory,False)
 MakeBar(Accuracy_Comp,Category,'Compensator',directory,False)
 MakeBar(BMI_Stats,Category,'BMI_stats',directory,True)
+
+#Histogram
+#Front Model
+ErrorAnalysis(Underweight_Values,Underweight_Prediction_F,len(Underweight),'Underweight','Front')
+ErrorAnalysis(Healthyweight_Values,Healthyweight_Prediction_F,len(Healthyweight),'Healthyweight','Front')
+ErrorAnalysis(Overweight_Values,Overweight_Prediction_F,len(Overweight),'Overweight','Front')
+ErrorAnalysis(Obese_Values,Obese_Prediction_F,len(Obese),'Obese','Front')
+#Side Model
+ErrorAnalysis(Underweight_Values,Underweight_Prediction_S,len(Underweight),'Underweight','Side')
+ErrorAnalysis(Healthyweight_Values,Healthyweight_Prediction_S,len(Healthyweight),'Healthyweight','Side')
+ErrorAnalysis(Overweight_Values,Overweight_Prediction_S,len(Overweight),'Overweight','Side')
+ErrorAnalysis(Obese_Values,Obese_Prediction_S,len(Obese),'Obese','Side')
+#Compensator
+ErrorAnalysis(Underweight_Values,Underweight_Prediction_C,len(Underweight),'Underweight','Compensator')
+ErrorAnalysis(Healthyweight_Values,Healthyweight_Prediction_C,len(Healthyweight),'Healthyweight','Compensator')
+ErrorAnalysis(Overweight_Values,Overweight_Prediction_C,len(Overweight),'Overweight','Compensator')
+ErrorAnalysis(Obese_Values,Obese_Prediction_C,len(Obese),'Obese','Compensator')
+
+#total dataset
+total = np.concatenate((Underweight_Values,Healthyweight_Values,Overweight_Values,Obese_Values))
+print(total.shape)
+print(total[0:10])
+total_front = np.concatenate((Underweight_Prediction_F,Healthyweight_Prediction_F,Overweight_Prediction_F,Obese_Prediction_F))
+total_front = total_front[:,0]
+print(total_front.shape)
+print(total_front[0:10])
+total_side = np.concatenate((Underweight_Prediction_S,Healthyweight_Prediction_S,Overweight_Prediction_S,Obese_Prediction_S))
+total_side = total_side[:,0]
+total_comp = np.concatenate((Underweight_Prediction_C,Healthyweight_Prediction_C,Overweight_Prediction_C,Obese_Prediction_C))
+total_comp = total_comp[:,0]
+TotalError(total,total_front,'Front')
+TotalError(total,total_side,'Side')
+TotalError(total,total_comp,'Compensator')
+
